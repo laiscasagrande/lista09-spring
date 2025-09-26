bb78c34af3573a2bfec1648267ab8fb023fb809f
Lais Kaminski Casagrande <lais.330323@alunosatc.edu.br>
2025-09-26 20:30:29 -0300

    continuação dos exercícios

--- lista09/src/main/java/com/example/lista09/FipeController.java
+++ lista09/src/main/java/com/example/lista09/FipeController.java
@@ -1,4 +1,20 @@
 package com.example.lista09;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/fipe") //rota base
 public class FipeController {
+
+    private RestClient cliente = RestClient.create("https://parallelum.com.br/fipe/api/v1/carros"); //api que irei utilizar
+
+    @GetMapping("/")
+    public String exibeTodasMarcas() {
+        return cliente.get().uri("/marcas").retrieve().body(String.class);
+    }
+
+
 }
